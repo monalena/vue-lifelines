7b7b95e0d9dd88a422c939b052020f76984520b7
Monika Schwarz <monika.schwarz@monash.edu>
2021-06-15 09:49:48 +1000

    Add Legend to visualisation, add death date to description

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="p-3 mb-2" id="cards">
+    <div class="p-3 mb-2" id="cards" v-if="!!specs[0]">
         <h4>Description</h4>
-
         <div>Convict Name: {{specs[0].GivenNames}} {{specs[0].FamilyName}}</div>
         <div>Convict ID: {{specs[0].ConvictId}}</div>
         <div>Ship: {{specs[0].ShipNameToVDL}}</div>
         <div v-if="!!specs[0].TradeFirst">Occupation: {{specs[0].TradeFirst}}</div>
         <div v-if="!!specs[0].BirthDateYear">Birth Year: {{specs[0].BirthDateYear}}</div>
+        <div v-if="!!specs[0].DeathDateYear">Death Year: {{specs[0].DeathDateYear}}</div>
         <br/>
     </div>
 </template>
---
+++ src/components/Legend.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="col-sm-12 ml-40" id="legend">
+        <h5>Legend</h5>
+        <div class="row">
+            <div class="col-sm-5 ml-4">
+                <h6>Offences against</h6>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <div><span class="dot" id="I"></span></div>
+                        <div><span class="dot" id="II"></span></div>
+                        <div><span class="dot" id="III"></span></div>
+                        <div><span class="dot" id="IV"></span></div>
+                        <div><span class="dot" id="V"></span></div>
+                    </div>
+                    <div class="col-sm-10">
+                        <div>Person - Class I</div>
+                        <div>Property - Class II</div>
+                        <div>Currency - Class III</div>
+                        <div>Good Order - Class IV</div>
+                        <div>Convict Regulation - Class V</div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-sm-5 ml-4">
+                <h6>Sentences</h6>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <div><span class="square" id="factory"></span></div>
+                        <div><span class="square" id="crime"></span></div>
+                        <div><span class="square" id="labour"></span></div>
+                        <div><span class="square" id="separate"></span></div>
+                        <div><span class="square" id="solitary"></span></div>
+                    </div>
+                    <div class="col-sm-10">
+                        <div>Factory</div>
+                        <div>Crime Class</div>
+                        <div>Hard Labour</div>
+                        <div>Separate Treatment</div>
+                        <div>Solitary Confinement</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Legend"
+    }
+</script>
+
+<style scoped>
+    #legend {
+        border-style: solid;
+        border-width: 1px;
+        border-radius: 2px;
+    }
+    text {
+        font-size: xx-small;
+        font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+    }
+    h5 {
+        text-align: center;
+    }
+    h6 {
+        text-align: center;
+    }
+    .dot {
+        border-style: solid;
+        border-width: 1px;
+        height: 12px;
+        width: 12px;
+        border-radius: 50%;
+        display: inline-block;
+    }
+    .square {
+        border-style: solid;
+        border-width: 1px;
+        height: 10px;
+        width: 10px;
+        border-radius: 10%;
+        display: inline-block;
+    }
+    #I {
+        background-color: #EFB366;
+    }
+    #II {
+        background-color: #8AB17D;
+    }
+    #III {
+        background-color: #264653;
+    }
+    #IV {
+        background-color: #C64C2E;
+    }
+    #V {
+        background-color: #287271;
+    }
+    #factory {
+        background-color: #2a9d8f;
+    }
+    #crime {
+        background-color: #e76f51;
+    }
+    #labour {
+        background-color: #e9c46a;
+    }
+    #separate {
+        background-color: #D3D3D3;
+    }
+    #solitary {
+        background-color: #000000;
+    }
+
+</style>
