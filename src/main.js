7d0953cf89fc4d236ad8fdc174d858e46beed3ff
Monika Schwarz <monika.schwarz@monash.edu>
2021-06-15 09:51:00 +1000

    Add vue-fontAwesome to project (not used right now, but maybe useful for legend later)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGavel, faCut, faHeart } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faGavel, faCut, faHeart)
+
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
